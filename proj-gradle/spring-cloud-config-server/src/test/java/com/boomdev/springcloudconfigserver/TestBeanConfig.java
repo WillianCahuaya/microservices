9d11d6faf8bb806481a333ac7c49b2894c1d8293
WILLY <wcahuayaramos@gmail.com>
2019-06-21 01:05:24 -0500

    application yml

--- proj-gradle/spring-cloud-config-server/src/test/java/com/boomdev/springcloudconfigserver/TestBeanConfig.java
+++ proj-gradle/spring-cloud-config-server/src/test/java/com/boomdev/springcloudconfigserver/TestBeanConfig.java
@@ -3,9 +3,11 @@ package com.boomdev.springcloudconfigserver;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest
 public class TestBeanConfig {
     @Autowired
     private BeanConfig beanConfig;
