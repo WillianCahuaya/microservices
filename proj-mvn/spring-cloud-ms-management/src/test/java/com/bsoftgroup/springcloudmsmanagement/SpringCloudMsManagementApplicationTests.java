aa3c2b151fecb5a0f3caf9c4b54922daf7ce39f4
Willian Cahuaya <wcahuayaramos@gmail.com>
2019-06-24 21:50:17 -0500

    project of maven

---
+++ proj-mvn/spring-cloud-ms-management/src/test/java/com/bsoftgroup/springcloudmsmanagement/SpringCloudMsManagementApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bsoftgroup.springcloudmsmanagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudMsManagementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
