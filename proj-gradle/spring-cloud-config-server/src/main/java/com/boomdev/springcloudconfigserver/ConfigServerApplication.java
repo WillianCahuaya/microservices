9213b69230690d76e3ab327e10bece4a48b76d24
WILLY <wcahuayaramos@gmail.com>
2019-06-27 22:21:02 -0500

    all project was updated

--- proj-gradle/spring-cloud-config-server/src/main/java/com/boomdev/springcloudconfigserver/ConfigServerApplication.java
+++ proj-gradle/spring-cloud-config-server/src/main/java/com/boomdev/springcloudconfigserver/ConfigServerApplication.java
@@ -1,10 +1,11 @@
 package com.boomdev.springcloudconfigserver;
 
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-//@EnableConfigServer
+@EnableConfigServer
 @ComponentScan({"com.boomdev"})
 public class ConfigServerApplication {
 }
--- proj-gradle/spring-cloud-config-server/src/test/java/com/boomdev/springcloudconfigserver/util/TestReadConfigFilePropertySource.java
+++ proj-gradle/spring-cloud-config-server/src/test/java/com/boomdev/springcloudconfigserver/util/TestReadConfigFilePropertySource.java
@@ -1,6 +1,5 @@
 package com.boomdev.springcloudconfigserver.util;
 
-import com.boomdev.springcloudconfigserver.util.ReadConfigFilePropertySource;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
+@RunWith(.class)
 @SpringBootTest
 public class TestReadConfigFilePropertySource {
 
