f83ed7c4681fdcb817bcf2ce8c983d46866be36e
WILLY <wcahuayaramos@gmail.com>
2019-07-01 02:33:32 -0500

    actuator

--- proj-gradle/online-sale-ms-management/src/main/java/com/boomdev/onlinesale/onlinesalemsmanagement/connection/DataConnectionController.java
+++ proj-gradle/online-sale-ms-management/src/main/java/com/boomdev/onlinesale/onlinesalemsmanagement/connection/DataConnectionController.java
@@ -1,10 +1,12 @@
 package com.boomdev.onlinesale.onlinesalemsmanagement.connection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class DataConnectionController {
 
     @Autowired
