7735421de8ef5257f981f15125eedcc692cfff83
Willian Cahuaya <wcahuayaramos@gmail.com>
2019-07-01 20:51:48 -0500

    other

---
+++ help/spring-cloud-management-properties/src/main/java/com/bsoftgroup/springcloudmanagementproperties/conexion/DatosConexionController.java
@@ -0,0 +1,22 @@
+package com.bsoftgroup.springcloudmanagementproperties.conexion;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DatosConexionController {
+	
+	
+	@Autowired
+	private ConfiguracionDataSource service;
+	
+	@GetMapping(path="/datosconexion")
+	public DatosConexion getDatosConexion() {
+		DatosConexion datosconexion = new DatosConexion(service.getDriverclassname(), service.getUrl(),
+				service.getUsername(), service.getPassword());
+		return datosconexion;
+	}
+	
+
+}
