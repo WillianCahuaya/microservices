d2840965d7aac240b5fbb46af40a816f07f1b1f5
WILLY <wcahuayaramos@gmail.com>
2019-07-10 21:53:34 -0500

    new query project

---
+++ proj-gradle/online-sale-ms-business-query/src/main/java/com/boomdev/onlinesale/onlinesalemsbusinessquery/OnlineSaleMsBusinessQueryApplication.java
@@ -0,0 +1,13 @@
+package com.boomdev.onlinesale.onlinesalemsbusinessquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineSaleMsBusinessQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineSaleMsBusinessQueryApplication.class, args);
+	}
+
+}
