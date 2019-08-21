2d88d696f611acabaed1100165261e480df65fa9
Willian Cahuaya <wcahuaya@buyersedgepurchasing.com>
2019-08-20 22:29:43 -0500

    delete demo

--- proj-mvn/spring-cloud-ms-security/src/main/java/com/bsoftgroup/springcloudmssecurity/dao/UserDetailDaoInterface.java
+++
@@ -1,9 +0,0 @@
-package com.bsoftgroup.springcloudmssecurity.dao;
-
-import com.bsoftgroup.springcloudmssecurity.bean.Usuario;
-
-public interface UserDetailDaoInterface {
-	
-	public Usuario getUsuario(String username);
-
-}
